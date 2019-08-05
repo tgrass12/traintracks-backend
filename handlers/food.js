4f9e1095e829e581ec60b38c8a80ddba6fd77fb4
Tyler Grass <twgrass12@gmail.com>
2019-08-04 23:01:57 -0700

    Add try-catch to food handler

--- handlers/food.js
+++ handlers/food.js
@@ -3,23 +3,28 @@ const Food = require('../models/Food');
 
 /* Get all foods in the Foods collection */
 module.exports.getAllFoods = async function(req, res, next) {
-	let foods = await Food.find({}).exec((err, foods) => {
-		if (err) res.status(500).json(err);
-
+	try {
+		let foods = await Food.find({});
 		res.json(foods);
-	});
+	}
+	catch (err) {
+		next(err)
+	}
 }
 
 /* Add a new food to the Foods collection */
 module.exports.addFood = async function(req, res, next) {
 	let {name, calories, macros} = req.body;
 	
-	let food = await Food.create({
-		'name': name,
-		'calories': calories,
-		'macros': macros
-	}).exec((err, newFood) => {
-		if (err) res.status(500).json(err);
+	try {
+		let newFood = await Food.create({
+			'name': name,
+			'calories': calories,
+			'macros': macros
+		});
 		res.json(newFood);
-	});
+	}
+	catch(err) {
+		next(err);
+	}
 };
