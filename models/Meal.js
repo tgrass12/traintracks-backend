7c90ddb3d1edddaa1e27623bcc575e1a030a4703
Tyler Grass <twgrass12@gmail.com>
2019-08-04 23:01:35 -0700

    Rename Diet to Meal

--- models/Meal.js
+++ models/Meal.js
@@ -2,25 +2,28 @@ const mongoose = require('mongoose');
 const Nutrients = require('./Nutrients');
 const Food = require('./Food');
 
-const dietSchema = new mongoose.Schema({
+const mealSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		default: 'Lunch'
+	},
 	foods: [{
 		'food': { 
 			type: mongoose.Schema.Types.ObjectId, 
 			ref: 'food'
 		},
-		'quantity': {
+		'servings': {
 			type: Number,
 			default: 1
 		}
 	}],
-	goals: Nutrients.schema,
 	total:  Nutrients.schema,
 	manual: {
 		type: Nutrients.schema,
 		default: Nutrients.schema
 	}
-}, {'_id': false});
+});
 
-const Diet  = mongoose.model('diet', dietSchema);
+const Meal  = mongoose.model('meal', mealSchema);
 
-module.exports = Diet;
+module.exports = Meal;
