68fdf5ba907a61baec04672ed0659bd4f2478890
Tyler Grass <twgrass12@gmail.com>
2019-10-09 16:08:55 -0700

    Create JournalEntry on workout log if one doesn't exist

--- handlers/workoutLog.js
+++ handlers/workoutLog.js
@@ -2,6 +2,32 @@ const LoggedExercise = require('../models/LoggedExercise');
 const User = require('../models/User');
 const JournalEntry = require('../models/JournalEntry');
 
+const createEntry = async function(user, date) {
+	let entry = await JournalEntry.create({ 
+		'user': user._id, 
+		'date': date,
+		'nutrition': {
+			'targets': user.targets.diet,
+			'meals': user.meals.map(m => { return { 'name': m } }),
+			'logged': {
+				'cals': 0,
+				'macros': {
+					'protein': 0,
+					'carbs': {
+						'total': 0
+					},
+					'fats': {
+						'total': 0
+					}
+				}
+			},
+			'water': 0	
+		}
+	});
+	await entry.save();
+	return entry;
+}
+
 module.exports.getWorkoutLog = async function(req, res, next) {
 	let { date } = req.params;
 	let user = req.user;		
@@ -35,12 +61,19 @@ module.exports.addExerciseToLog = async function(req, res, next) {
 			reps: reps
 		});
 
-		let entry = await JournalEntry.findOneAndUpdate(
-			{ 'user': user.id, 'date': date },
-			{
+		let entry = await JournalEntry.findOne(
+			{ 'user': user._id, 'date': date }
+		);
+
+		if (!entry) {
+			entry = await createEntry(user, date);
+		}
+		
+		entry = await JournalEntry.findOneAndUpdate(
+			{ 'user': user._id, 'date': date },
+			{			
 				$push: { 'workouts': exercise }
-			},
-			{ 'upsert': true, 'new': true }
+			}
 		);
 
 		res.json(exercise);
