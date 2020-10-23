a3dfd95c7bcf7a812695794ed1dc7d96e0a77010
Tyler Grass <twgrass12@gmail.com>
2020-10-23 02:32:00 -0700

    Add query to get journal by username and date

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,7 +6,23 @@ function getFoodById(parent, args, ctx) {
   return ctx.prisma.food.findOne({ where: { id: Number(args.id) } });
 }
 
+async function getJournalByUsernameAndDate(parent, args, ctx) {
+  const { id: userId } = await ctx.prisma.user.findOne({
+    where: { username: args.username },
+  });
+
+  return ctx.prisma.journalEntry.findOne({
+    where: {
+      userEntryDateUnique: {
+        userId,
+        entryDate: args.date,
+      },
+    },
+  });
+}
+
 module.exports = {
   user,
   getFoodById,
+  getJournalByUsernameAndDate,
 };
